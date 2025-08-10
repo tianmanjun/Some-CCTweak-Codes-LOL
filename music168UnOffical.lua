1f200a46f7c896cb9ca51b8dff3c5b9a755f7fec
tianmanjun <83486194+tianmanjun@users.noreply.github.com>
2025-08-10 13:35:39 +0800

    Update music168UnOffical.lua

--- music168UnOffical.lua
+++ music168UnOffical.lua
@@ -6,7 +6,7 @@ local mypath = "/" .. fs.getDir(shell.getRunningProgram())
 
 if not fs.exists(mypath .. "/lib/basalt.lua") then shell.run(
     "wget http://alist.liulikeji.cn/d/HFS/Installer/lib/basalt.lua lib/basalt.lua") end
-if not fs.exists(mypath .. "/speaker.lua") then shell.run("wget http://alist.liulikeji.cn/d/HFS/music168/speaker.lua") end
+if not fs.exists(mypath .. "/speakerUnOffical.lua") then shell.run("wget https://raw.githubusercontent.com/tianmanjun/Some-CCTweak-Codes-LOL/refs/heads/main/speakerUnOffical.lua") end
 
 --*GUI库导入
 basalt            = require(mypath .. "/lib/basalt")
@@ -319,7 +319,7 @@ function Search(input_str, GUI_in, api)
                     frame = play_lib_F:addFrame():setPosition(2, a):setSize("parent.w-2", 3):setBackground(colors
                     .lightBlue):onClick(function()
                         if play_data_table["play"] then
-                            shell.run(mypath .. "/speaker.lua stop")
+                            shell.run(mypath .. "/speakerUnOffical.lua stop")
                             if _G.Playopen then end
                             _G.music168_playopen = false
                             play_data_table["play"] = false
@@ -393,7 +393,7 @@ function thread2()
             if play_data_table["play_table_index"] ~= play_table_Gui[3]:getItemIndex() then
                 index = play_table_Gui[3]:getItemIndex()
                 if play_data_table["play"] then
-                    shell.run(mypath .. "/speaker.lua stop")
+                    shell.run(mypath .. "/speakerUnOffical.lua stop")
                     if _G.Playopen then
 
                     end
@@ -457,7 +457,7 @@ function speakerp()
             end
             time = GetmusicTime(_G.music168_music_id)
             play_Gui[12]:setText(string.format("%02d",math.floor(time/60000))..":"..string.format("%02d",math.floor((time - math.floor(time/60000)*60000))/1000))
-            shell.run(mypath .. "/speaker play " .. dfpwmURL)
+            shell.run(mypath .. "/speakerUnOffical play " .. dfpwmURL)
             play_set_0()
         end
     end
