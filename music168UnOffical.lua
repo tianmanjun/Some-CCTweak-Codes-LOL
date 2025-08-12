4535fa63953eb231275c12e37786c2fb5af8fba5
tianmanjun <83486194+tianmanjun@users.noreply.github.com>
2025-08-12 22:32:20 +0800

    Update music168UnOffical.lua

--- music168UnOffical.lua
+++ music168UnOffical.lua
@@ -388,8 +388,8 @@ function Search(input_str, GUI_in, api)
                 table_get2 = textutils.unserialiseJSON(json_str2)
                 if httpGetFromID and table_get2["songs"][1] then
                     kg_d1 = true
-                    name,sn = pinyin(GetOffCharas(table_get2["songs"][1]["name"]),true,'')
-                    arname,san = pinyin(GetOffCharas(table_get2["songs"][1]["ar"]["name"]),true,'')
+                    name,sn = pinyin(GetOffCharas(table_get2["songs"][1]["name"] or "NULL"),true,'')
+                    arname,san = pinyin(GetOffCharas(table_get2["songs"][1]["ar"][1]["name"] or "NULL"),true,'')
                     output2_table = { ["id"] = tonumber(input_str), ["name"] = name , ["artists_id"] = table_get2["songs"][1]["ar"]["id"], ["artists_name"] = arname }
                     
                 end
